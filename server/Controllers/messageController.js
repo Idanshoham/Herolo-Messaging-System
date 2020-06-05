e0e59c64b2e87ff8c2aee563a6a4a4937d2e4a5d
Idan Shoham <Idanshoham1@gmail.com>
2020-06-05 08:22:50 +0300

    initiate server-side

---
+++ server/Controllers/messageController.js
@@ -0,0 +1,8 @@
+exports.writeMessage = function(req, res, next) {
+}
+
+exports.deleteMessage = function(req, res, next) {
+}
+
+exports.getAllMessages = function(req, res, next) {
+}
--- server/router.js
+++ server/router.js
@@ -1,3 +1,17 @@
-module.exports = function(app) {
+const Authentication = require('./controllers/authentication');
+const MessageController = require('./controllers/messageController');
+const passportService = require('./services/passport');
+const passport = require('passport');
 
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
+
+module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    });
+    app.post('/signin', requireSignin ,Authentication.signin);
+    app.post('/signup', Authentication.signup);
+
+    //TODO: add message functions here.
 };
