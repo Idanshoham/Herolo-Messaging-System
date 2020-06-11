8152a9a78106cc282f88f0a4e429beed4510241b
Idan Shoham <Idanshoham1@gmail.com>
2020-06-11 14:17:17 +0300

    update server file

--- server.js
+++ server.js
@@ -11,19 +11,32 @@ const cors = require('cors');
 // DB setup
 mongoose.connect('mongodb://localhost:27017');
 
+// Dev app setup
+if (app.get('env') === 'development') {
+    app.use(morgan('combined'));
+    app.use(cors());
+}
+
 // App setup
-app.use(morgan('combined'));
-app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
+app.use(bodyParser.urlencoded({ extended: false }));
 router(app);
 
-// Production app
+// Production app setup
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'build')));
 	app.get('*', function (req, res) {
 		res.sendFile(path.join(__dirname, 'build', 'index.html'));
 	});
 }
+
+// Error handling
+app.use(function (req, res) {
+	res.status(404).send('Page not found');
+});
+app.use(function (error, req, res, next) {
+	res.status(500).send(`An error has occured: ${error}`);
+});
 
 // Server setup
 const port = process.env.PORT || 5000;
