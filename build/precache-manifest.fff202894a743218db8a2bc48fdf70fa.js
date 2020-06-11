d7830c19623992c3593074d44a5829d54ca83511
Idan Shoham <Idanshoham1@gmail.com>
2020-06-11 13:26:55 +0300

    build

---
+++ build/precache-manifest.fff202894a743218db8a2bc48fdf70fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8ba29d11bd8b3aafc0bb3d5d8ba881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ac170adaa9d5d7b793",
+    "url": "/static/js/2.cc21cc3b.chunk.js"
+  },
+  {
+    "revision": "199332de35db97ad3352feb220e32cbf",
+    "url": "/static/js/2.cc21cc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d069a988107e8ce01756",
+    "url": "/static/js/main.bb7e9098.chunk.js"
+  },
+  {
+    "revision": "04fb22dc5ba9e28e5803",
+    "url": "/static/js/runtime-main.6cb3b8af.js"
+  }
+]);
